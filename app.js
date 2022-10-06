bfebb8616e981cb895ef31ef676eef69598f0bb4
Stella Ling <stellalph2@gmail.com>
2022-10-06 23:24:00 +1030

    add DomContentLoadedEvent code

--- app.js
+++ app.js
@@ -13,49 +13,54 @@
 // 6. Auto focus after add.
 // 7. Enable Enter-to-submit functionality
 
-const form = document.querySelector('form.add');
-const addButton = document.querySelector('#addButton');
-const itemToAdd = document.querySelector('#itemToAdd');
-const ul = document.querySelector('.todo');
-const body = document.querySelector('body');
-const app = document.querySelector('.app');
-
-app.addEventListener('click', function(event){
-    event.stopPropagation();
-   
-})
-body.addEventListener('click', function(event){
-    ul.innerHTML = '';  
-})
-
-
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-    if(itemToAdd.value){
-        const newItem = createItem(itemToAdd.value);
-        ul.appendChild(newItem);
-        itemToAdd.value = '';
-        itemToAdd.focus()
-    }
-   
-  
-});
-
-function createItem(val) {
-    const item = document.createElement('li');
-    const span = document.createElement('span');
-    const delBtn = document.createElement('button');
-
-    span.textContent = val;
-    delBtn.textContent = 'Delete';
-    delBtn.classList.add('btn-link');
-
-    item.appendChild(span);
-    item.appendChild(delBtn);
-
-    delBtn.addEventListener('click', function () {
-        item.parentNode.removeChild(item);
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.querySelector('form.add');
+    const addButton = document.querySelector('#addButton');
+    const itemToAdd = document.querySelector('#itemToAdd');
+    const ul = document.querySelector('.todo');
+    const body = document.querySelector('body');
+    const app = document.querySelector('.app');
+    
+    app.addEventListener('click', function(event){
+        event.stopPropagation();
+       
     })
+    body.addEventListener('click', function(event){
+        ul.innerHTML = '';  
+    })
+    
+    
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        if(itemToAdd.value){
+            const newItem = createItem(itemToAdd.value);
+            ul.appendChild(newItem);
+            itemToAdd.value = '';
+            itemToAdd.focus()
+        }
+       
+      
+    });
+    
+    function createItem(val) {
+        const item = document.createElement('li');
+        const span = document.createElement('span');
+        const delBtn = document.createElement('button');
+    
+        span.textContent = val;
+        delBtn.textContent = 'Delete';
+        delBtn.classList.add('btn-link');
+    
+        item.appendChild(span);
+        item.appendChild(delBtn);
+    
+        delBtn.addEventListener('click', function () {
+            item.parentNode.removeChild(item);
+        })
+    
+        return item;
+    }
+})
 
-    return item;
-}
+
+
